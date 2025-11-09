dd55373ca7ea01caf583600bb02bc6191b3137ea
Kkaif4 <120726421+Kkaif4@users.noreply.github.com>
2025-11-09 04:23:21 -0500

    Update API_BASE_URL to use AWS endpoint

--- src/component/api/api.js
+++ src/component/api/api.js
@@ -1,3 +1,4 @@
 // export const API_BASE_URL = "http://82.180.163.53:4096/api";
 
-export const API_BASE_URL = "http://localhost:9090/api";
+export const API_BASE_URL = "https://ec2-98-89-29-113.compute-1.amazonaws.com:9090/api";
+
