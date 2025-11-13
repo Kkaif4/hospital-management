a720ecc59ffdc607e6ef5933a9cc6edc8e3a4e59
Kkaif4 <120726421+Kkaif4@users.noreply.github.com>
2025-11-13 06:56:05 -0500

    Update api.js

--- src/component/api/api.js
+++ src/component/api/api.js
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = "http://82.180.163.53:4096/api";
 
-export const API_BASE_URL = "http://ec2-98-89-29-113.compute-1.amazonaws.com:9090/api";
+export const API_BASE_URL = "https://ec2-98-89-29-113.compute-1.amazonaws.com:9090/api";
